0a744ae346cc86c890104825097e9ed0f705b0da
sss2016 <1079783140@qq.com>
2018-09-06 15:58:08 +0800

    alter wsgi.py

--- v_street/wsgi.py
+++ v_street/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for v_street project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'v_street.settings')
+import sys
+sys.path.append("/var/www/v_street/")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "v_street.settings")
 
 application = get_wsgi_application()
